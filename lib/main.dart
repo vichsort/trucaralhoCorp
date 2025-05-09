66d3835301dfc8ed906c59907e15552eecce3a30
Vitor Mignoni <vichsort@proton.me>
2025-05-09 18:39:48 -0300

    click dir / esq

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const Trucaralho());
+  runApp(const TrucaralhoApp());
 }
 
-class Trucaralho extends StatelessWidget {
-  const Trucaralho({super.key});
+class TrucaralhoApp extends StatelessWidget {
+  const TrucaralhoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Trucaralho',
+      title: 'Trucaralho Click Counter',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
-        textTheme: GoogleFonts.nunitoSansTextTheme(),
+        primarySwatch: Colors.blue,
       ),
-      home: const TrucoPage(title: 'Truco'),
+      home: const ClickCounterPage(),
     );
   }
 }
 
-class TrucoPage extends StatefulWidget {
-  const TrucoPage({super.key, required this.title});
-
-  final String title;
+class ClickCounterPage extends StatefulWidget {
+  const ClickCounterPage({Key? key}) : super(key: key);
 
   @override
-  State<TrucoPage> createState() => _TrucoPageState();
+  State<ClickCounterPage> createState() => _ClickCounterPageState();
 }
 
-class _TrucoPageState extends State<TrucoPage> {
-  int _counter = 0;
+class _ClickCounterPageState extends State<ClickCounterPage> {
+  int _leftCount = 0;
+  int _rightCount = 0;
 
-  void _incrementCounter() {
+  void _incrementLeft() {
     setState(() {
-      _counter++;
+      _leftCount++;
+    });
+  }
+
+  void _incrementRight() {
+    setState(() {
+      _rightCount++;
     });
   }
 
@@ -43,26 +46,48 @@ class _TrucoPageState extends State<TrucoPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.card_giftcard),
-        title: Text(widget.title),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.abc))],
+        title: const Text('Trucaralho Click Counter'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Trco ne viado'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+      body: Row(
+        children: [
+          Expanded(
+            child: GestureDetector(
+              onTap: _incrementLeft,
+              child: Container(
+                color: Colors.blue.withOpacity(0.1),
+                child: Center(
+                  child: Text(
+                    'Esquerda: $_leftCount',
+                    style: const TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: _incrementRight,
+              child: Container(
+                color: Colors.red.withOpacity(0.1),
+                child: Center(
+                  child: Text(
+                    'Direita: $_rightCount',
+                    style: const TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
