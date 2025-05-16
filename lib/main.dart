a14bd2bb7c4c20de207fc9d03a93e8f45a6f8750
PietroMatheus <140650026+PietroMatheus@users.noreply.github.com>
2025-05-16 20:16:30 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,10 @@ class _ClickCounterPageState extends State<ClickCounterPage> {
   void _incrementLeft() {
     setState(() {
       _leftCount += up;
+      up = 1;
       if (_leftCount >= 12) {
         _leftCount = 0;
+        _rightCount = 0;
         _leftWins++;
 
         ScaffoldMessenger.of(context).showSnackBar(
@@ -82,8 +84,10 @@ class _ClickCounterPageState extends State<ClickCounterPage> {
   void _incrementRight() {
     setState(() {
       _rightCount += up;
+      up = 1;
 
       if (_rightCount >= 12) {
+        _leftCount = 0;
         _rightCount = 0;
         _rightWins++;
 
@@ -134,10 +138,16 @@ class _ClickCounterPageState extends State<ClickCounterPage> {
     });
   }
 
+  ValueNotifier<bool> isDialOpen = ValueNotifier(false);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Trucaralho'), centerTitle: true),
+      appBar: AppBar(
+        title: const Text('Trucaralho'),
+        centerTitle: true,
+        leading: Image.asset('assets/images/trucaralho_icon.png'),
+      ),
       body: Row(
         children: [
           // nos
@@ -245,6 +255,7 @@ class _ClickCounterPageState extends State<ClickCounterPage> {
       ),
 
       floatingActionButton: SpeedDial(
+        openCloseDial: isDialOpen,
         icon: Icons.add,
         activeIcon: Icons.close,
         backgroundColor: Colors.blue,
@@ -261,8 +272,278 @@ class _ClickCounterPageState extends State<ClickCounterPage> {
             child: const Icon(Icons.card_travel_sharp),
             backgroundColor: Colors.deepOrange,
             foregroundColor: Colors.white,
-            label: 'BlackJack',
-            onTap: () => debugPrint('SECOND CHILD'),
+            label: 'Blackaralho',
+            onTap:
+                () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Blackjack()),
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// blackjack
+
+class Blackjack extends StatefulWidget {
+  const Blackjack({Key? key}) : super(key: key);
+
+  @override
+  State<Blackjack> createState() => _BlackjackState();
+}
+
+class _BlackjackState extends State<Blackjack> {
+  int _leftCount = 0;
+  int _leftWins = 0;
+  int _rightCount = 0;
+  int _rightWins = 0;
+  int up = 1;
+
+  void _incrementLeft() {
+    setState(() {
+      _leftCount += up;
+      up = 1;
+      if (_leftCount >= 12) {
+        _leftCount = 0;
+        _rightCount = 0;
+        _leftWins++;
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text('Nós ganhamos!'),
+            duration: const Duration(seconds: 1),
+          ),
+        );
+      }
+    });
+  }
+
+  void _decreaseLeft() {
+    setState(() {
+      _leftCount--;
+      if (_leftCount <= 0) {
+        _leftCount = 0;
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text('Não pode diminuir mais!'),
+            duration: const Duration(seconds: 1),
+          ),
+        );
+      }
+    });
+  }
+
+  void _decreaseRight() {
+    setState(() {
+      _rightCount--;
+      if (_rightCount <= 0) {
+        _rightCount = 0;
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text('Não pode diminuir mais!'),
+            duration: const Duration(seconds: 1),
+          ),
+        );
+      }
+    });
+  }
+
+  void _incrementRight() {
+    setState(() {
+      _rightCount += up;
+      up = 1;
+
+      if (_rightCount >= 12) {
+        _leftCount = 0;
+        _rightCount = 0;
+        _rightWins++;
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text('Eles ganharam!'),
+            duration: const Duration(seconds: 1),
+          ),
+        );
+      }
+    });
+  }
+
+  void _changeUp() {
+    setState(() {
+      if (up == 1) {
+        up = 3;
+      } else if (up == 3) {
+        up = 6;
+      } else if (up == 6) {
+        up = 9;
+      } else if (up == 9) {
+        up = 12;
+      } else {
+        up = 1;
+      }
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _leftCount = 0;
+      _rightCount = 0;
+      _leftWins = 0;
+      _rightWins = 0;
+    });
+  }
+
+  void _resetLeft() {
+    setState(() {
+      _leftCount = 0;
+    });
+  }
+
+  void _resetRight() {
+    setState(() {
+      _rightCount = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Blackaralho'),
+        centerTitle: true,
+        leading: Image.asset('assets/images/icon.png'),
+      ),
+      body: Row(
+        children: [
+          // convidados
+          Expanded(
+            child: GestureDetector(
+              onTap: _incrementLeft,
+              onDoubleTap: _decreaseLeft,
+              onLongPress: _resetLeft,
+              child: Container(
+                color: Colors.blue.shade100,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        '$_leftCount',
+                        style: const TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Text(
+                        "Convidados",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue.shade900,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      const Icon(Icons.favorite, size: 48, color: Colors.blue),
+                      const SizedBox(height: 8),
+                      Text(
+                        '$_leftWins',
+                        style: const TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // linha
+          Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: _changeUp, child: Text('$up')),
+              ElevatedButton(onPressed: _reset, child: Icon(Icons.refresh)),
+            ],
+          ),
+
+          // casa
+          Expanded(
+            child: GestureDetector(
+              onTap: _incrementRight,
+              onDoubleTap: _decreaseRight,
+              onLongPress: _resetRight,
+              child: Container(
+                color: Colors.red.shade100,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        '$_rightCount',
+                        style: const TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Text(
+                        "Casa",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red.shade900,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      const Icon(Icons.favorite, size: 48, color: Colors.red),
+                      const SizedBox(height: 8),
+                      Text(
+                        '$_rightWins',
+                        style: const TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      floatingActionButton: SpeedDial(
+        icon: Icons.add,
+        activeIcon: Icons.close,
+        backgroundColor: Colors.blue,
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.card_giftcard),
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            label: 'Fodinha',
+            onTap: () {},
+            onLongPress: () => debugPrint('FIRST CHILD LONG PRESS'),
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.card_travel_sharp),
+            backgroundColor: Colors.deepOrange,
+            foregroundColor: Colors.white,
+            label: 'Trucaralho',
+            onTap:
+                () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Blackjack()),
+                ),
           ),
         ],
       ),
